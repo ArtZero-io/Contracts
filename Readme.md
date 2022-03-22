480aee5831cb838324ced94ba92a59ed3f2e824b
Nad128668 <nad128668@gmail.com>
2022-03-22 22:32:49 +0700

    add enumerable Mapping, improve staking storage

--- Readme.md
+++ Readme.md
@@ -7,7 +7,7 @@ Contract Addresses:
 5FarkyYRSR2EH9UGhdshFFwSqJL99QtGL1QkeENHLUXW4XkA
 
 # ArtZero NFT Staking Contract
-5CAmTgzZj9rDjqqisBzPcGB2uG7DgBmGje2NsWYuwsZNSXxR
+5GL6jcGgcjBEKhXQhwFtm7BhPGQZZneBjoEeNqRNofCPQE3G
 
 # Collection Manager
 5GBBXatBR3F9jEcThxu8sTLQo9HHj99yNYLZKod4bVX9p8dz
--- contracts/OpenBrush-Staking-ArtZero-NFT/lib.rs
+++ contracts/OpenBrush-Staking-ArtZero-NFT/lib.rs
@@ -20,6 +20,48 @@ pub mod artzero_staking_nft {
     use ink_prelude::vec::Vec;
     use ink_storage::Mapping;
 
+    #[derive(Default, Debug, ink_storage::traits::SpreadLayout, ink_storage::traits::SpreadAllocate)]
+    #[cfg_attr(feature = "std", derive(ink_storage::traits::StorageLayout))]
+    pub struct EnumerableMapping {
+        /// Mapping from index to `Id`.
+        ///
+        /// ** Note ** Owner can be `None` that means it is a contract.
+        id_to_index: Mapping<(Option<AccountId>, u32), u32>,
+        /// Mapping from owner's index to `Id`.
+        ///
+        /// ** Note ** Owner can be `None` that means it is a contract.
+        index_to_id: Mapping<(Option<AccountId>, u32), u32>,
+    }
+
+    impl EnumerableMapping {
+        pub fn insert(&mut self, owner: &Option<AccountId>, id: &u32, index: &u32) {
+            self.id_to_index.insert((owner, id), index);
+            self.index_to_id.insert((owner, index), id);
+        }
+
+        pub fn remove(&mut self, owner: &Option<AccountId>, id: &u32, last_index: &u32) -> Result<(), PSP34Error> {
+            let index = self.id_to_index.get((owner, id)).ok_or(PSP34Error::TokenNotExists)?;
+
+            if last_index != &index {
+                let last_id = self
+                    .index_to_id
+                    .get((owner, last_index))
+                    .ok_or(PSP34Error::TokenNotExists)?;
+                self.index_to_id.insert((owner, &index), &last_id);
+                self.id_to_index.insert((owner, &last_id), &index);
+            }
+
+            self.index_to_id.remove((owner, &last_index));
+            self.id_to_index.remove((owner, id));
+
+            Ok(())
+        }
+
+        pub fn get_by_index(&self, owner: &Option<AccountId>, index: &u32) -> Result<u32, PSP34Error> {
+            self.index_to_id.get((owner, index)).ok_or(PSP34Error::TokenNotExists)
+        }
+    }
+
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
@@ -44,7 +86,8 @@ pub mod artzero_staking_nft {
     pub struct ArtZeroStakingNFT{
         #[OwnableStorageField]
         ownable: OwnableData,
-        staking_list: Mapping<AccountId, Vec<u32>>,
+        staking_list: EnumerableMapping,
+        staking_list_last_index: Mapping<Option<AccountId>, u32>,
         nft_contract_address: AccountId,
         total_staked: u32
     }
@@ -95,8 +138,8 @@ pub mod artzero_staking_nft {
         }
         ///Get staked token ids by AccountId
         #[ink(message)]
-        pub fn get_staked_ids(&self,account: AccountId) -> Option<Vec<u32>> {
-            self.staking_list.get(&account)
+        pub fn get_staked_id(&self,account: AccountId, index: u32) -> u32 {
+            self.staking_list.get_by_index(&Some(account),&index).unwrap()
         }
         ///Get total NFT staked in the contract
         #[ink(message)]
@@ -111,6 +154,13 @@ pub mod artzero_staking_nft {
             let caller = self.env().caller();
             let leng = token_ids.len();
 
+            let mut last_index = 0;
+            if self.staking_list_last_index.get(Some(caller)).is_some() {
+                last_index = self.staking_list_last_index.get(Some(caller)).unwrap();
+            }
+
+            self.total_staked = self.total_staked.checked_add(leng as u32).unwrap();
+
             for i in 0..leng{
                 //Step 1 - Check if the token is belong to caller
                 let token_owner = Psp34Ref::owner_of(&self.nft_contract_address,Id::U32(token_ids[i])).unwrap();
@@ -120,26 +170,8 @@ pub mod artzero_staking_nft {
                 let allowance = Psp34Ref::allowance(&self.nft_contract_address,caller, self.env().account_id(), Some(Id::U32(token_ids[i])));
                 assert!(allowance);
 
-            }
+                self.staking_list.insert(&Some(caller),&token_ids[i],&(last_index+(i as u32)+1));
 
-            self.total_staked = self.total_staked.checked_add(leng as u32).unwrap();
-
-            if self.staking_list.get(&caller).is_some()
-            {
-                let mut staked_ids:Vec<u32> = self.staking_list.get(&caller).unwrap();
-                for i in 0..leng{
-                    staked_ids.push(token_ids[i]);
-                }
-                self.staking_list.insert(&caller, &staked_ids);
-            }
-            else{
-                let mut new_staked_ids = Vec::<u32>::new();
-                for i in 0..leng{
-                    new_staked_ids.push(token_ids[i]);
-                }
-                self.staking_list.insert(&caller, &new_staked_ids);
-            }
-            for i in 0..leng {
                 //Step 3 - Transfer Token from Caller to Staking Contract
                 if !PSP34Ref::transfer_builder(&self.nft_contract_address, self.env().account_id(), Id::U32(token_ids[i]), Vec::<u8>::new())
                 .call_flags(CallFlags::default().set_allow_reentry(true))
@@ -151,6 +183,8 @@ pub mod artzero_staking_nft {
                     token_id:token_ids[i]
                 });
             }
+            self.staking_list_last_index.insert(Some(caller),&(last_index+leng as u32));
+
             Ok(())
         }
 
@@ -161,23 +195,15 @@ pub mod artzero_staking_nft {
             let caller = self.env().caller();
             let leng = token_ids.len();
 
+            assert!(self.staking_list_last_index.get(Some(caller)).is_some());
+
+            let mut last_index = self.staking_list_last_index.get(Some(caller)).unwrap();
+
             self.total_staked = self.total_staked.checked_sub(leng as u32).unwrap();
-            let mut token_indexes = Vec::<usize>::new();
-            let mut staked_ids:Vec<u32> = self.staking_list.get(&caller).unwrap();
 
             for i in 0..leng{
-                assert!(staked_ids.contains(&token_ids[i]));
-                token_indexes.push(staked_ids.iter().position(|&r| r == token_ids[i]).unwrap());
-            }
-
-            token_indexes.sort();
-            token_indexes.reverse();
-            for r in token_indexes {
-                staked_ids.remove(r);
-            }
-            self.staking_list.insert(&caller, &staked_ids);
-
-            for i in 0..leng{
+                assert!(self.staking_list.remove(&Some(caller),&token_ids[i],&last_index).is_ok());
+                last_index = last_index.checked_sub(1).unwrap();
                 //Step 2 - transfer token to caller
                 assert!(Psp34Ref::transfer(&self.nft_contract_address,caller,Id::U32(token_ids[i]),Vec::<u8>::new()).is_ok());
 
@@ -186,6 +212,29 @@ pub mod artzero_staking_nft {
                     token_id:token_ids[i]
                 });
             }
+            self.staking_list_last_index.insert(Some(caller),&last_index);
+
+            Ok(())
+        }
+
+        /// Withdraw Fees - only Owner
+        #[ink(message)]
+        #[modifiers(only_owner)]
+        pub fn withdraw_fee(&mut self,value: Balance)  -> Result<(), Error> {
+            assert!( value <= self.env().balance());
+            
+            if self.env().transfer(self.env().caller(), value).is_err() {
+                panic!(
+                    "error withdraw_fee"
+                )
+            }
+            Ok(())
+        }
+        ///Transfer NFT token
+        #[ink(message)]
+        #[modifiers(only_owner)]
+        pub fn tranfer_nft(&mut self, token_id: Id, receiver: AccountId) -> Result<(),Error> {
+            assert!(Psp34Ref::transfer(&self.nft_contract_address,receiver,token_id.clone(),Vec::<u8>::new()).is_ok());
             Ok(())
         }
     }
@@ -195,10 +244,11 @@ pub mod artzero_staking_nft {
         ///Get User NFT staked in the contract
         #[ink(message)]
         fn get_total_staked_by_account(&self,account: AccountId) -> u32 {
-            if self.staking_list.get(&account).is_none() {
-                return 0;
+            let last_index = self.staking_list_last_index.get(Some(account));
+            if last_index.is_some(){
+                return last_index.unwrap();
             }
-            self.staking_list.get(&account).unwrap().len() as u32
+            return 0;
         }
     }
 }
