ea4a1fd40ee957b1e1d71ea05577a5170bc7b413
hnnam0906 <namhoangnhat0906@gmail.com>
2023-02-20 10:19:31 +0100

    Add tests for marketplace

--- Azero_Contracts/tests/5.marketplace.ts
+++ Azero_Contracts/tests/5.marketplace.ts
@@ -16,31 +16,55 @@ import ConstructorsCollectionManager from './typed_contracts/constructors/artzer
 import ContractCollectionManager from './typed_contracts/contracts/artzero_collection_manager';
 import * as CollectionManagerArgs from './typed_contracts/types-arguments/artzero_collection_manager';
 
-describe('Profile test', () => {
+import {BN} from '@polkadot/util';
+
+describe('Marketplace test', () => {
     let api: any;
     let signers: any;
     let defaultSigner: any;
     let alice: any;
     let bob: any;
 
-    let nftContractAddress: any;
-    let standardNftHash: string;
-    let simpleModeAddingFee: string;
-    let advanceModeAddingFee: string;
-    let maxRoyaltyFeeRate: number;
-    
-    let stakingContractAddress: any;
-    let stakingContract: any;
-
-    let collectionManagerContractAddress: any;
-    let collectionManagerContract: any;
-
-    let platformFee: number;
     let contractAddress: any;
     let contract: any;
     let query: any;
-    let nftTx: any;
     let tx: any;
+
+    let platformFee: number;
+
+    let nftPmpContractAddress: any;
+    let nftPmpContract: any;
+    let nftPmpQuery: any;
+    let nftPmpTx: any;
+
+    let aliceNftContractAddress: any;
+    let aliceNftContract: any;
+    let aliceNftQuery: any;
+    let aliceNftTx: any;
+
+    let bobNftContractAddress: any;
+    let bobNftContract: any;
+    let bobNftQuery: any;
+    let bobNftTx: any;
+    
+    let stakingContractAddress: any;
+    let stakingContract: any;
+    let stakingQuery: any;
+    let stakingTx: any;
+
+    let stakingAdminAddress: string;
+    let stakingLimitUnstakeTime: number;
+
+    let cmContractAddress: any;
+    let cmContract: any;
+    let cmQuery: any;
+    let cmTx: any;
+
+    let cmAdminAddress: string;
+    let cmStandardNftHash: string;
+    let cmSimpleModeAddingFee: string;
+    let cmAdvanceModeAddingFee: string;
+    let cmMaxRoyaltyFeeRate: number;
 
     async function setup() {
         api = await ApiPromise.create({ provider });
@@ -53,39 +77,46 @@ describe('Profile test', () => {
         await checkAccountsBalance(signers, api);
 
         /**
-         * Step 1: Alice creates a Psp34 NFT contract
+         * Step 1: Alice creates a Psp34 PMP NFT contract
          */
         // "refTime: 769080524"
-        let nftContractGasLimit = setGasLimit(api, 1_600_000_000, 0);
-        const nftContractFactory = new ConstructorsPsp34Nft(api, alice);
+        let nftPmpContractGasLimit = setGasLimit(api, 1_600_000_000, 0);
+        const nftPmpContractFactory = new ConstructorsPsp34Nft(api, alice);
         
-        let nftContractOwner = alice.address;
-        let nftName = "Test PMP Staking";
-        let nftSymbol = "TPS";
+        let nftPmpContractOwner = alice.address;
+        let nftPmpName = "Test PMP Staking";
+        let nftPmpSymbol = "TPS";
 
-        stakingContractAddress = (
-            await nftContractFactory.new(
-                nftContractOwner,
-                nftName,
-                nftSymbol,
-                {gasLimit: nftContractGasLimit}
+        nftPmpContractAddress = (
+            await nftPmpContractFactory.new(
+                nftPmpContractOwner,
+                nftPmpName,
+                nftPmpSymbol,
+                {gasLimit: nftPmpContractGasLimit}
             )
         ).address;
 
+        // console.log("nftPmpContractAddress =", nftPmpContractAddress);
+
+        // PMP NFT contract
+        nftPmpContract = new ContractPsp34Nft(nftPmpContractAddress, alice, api);    
+        nftPmpQuery = nftPmpContract.query;
+        nftPmpTx = nftPmpContract.tx;
+
         /** 
-         * Step 2: Alice mints 2 NFTs and sends NFT tokenId #2 to Bob
+         * Step 2: Alice mints 2 PMP NFTs and sends NFT tokenId #2 to Bob
          */ 
-        // Mint 2 NFTs
-        await nftTx.mint(); // Mint #1
-        await nftTx.mint(); // Mint #2
-        const nftTokenId = PSP34Args.IdBuilder.U64(2); // Nft id 2
-        let nftData = new Array<number>();
+        // Mint 2 PMP NFTs
+        await nftPmpTx.mint(); // Mint #1
+        await nftPmpTx.mint(); // Mint #2
+        const nftPmpTokenId = PSP34Args.IdBuilder.U64(2); // Nft id 2
+        let nftPmpData = new Array<number>();
 
         // Transfer #2 to Bob
-        await nftTx.transfer(
+        await nftPmpTx.transfer(
             bob.address,
-            nftTokenId,
-            nftData
+            nftPmpTokenId,
+            nftPmpData
         );
 
         /** 
@@ -94,58 +125,161 @@ describe('Profile test', () => {
         let stakingContractGasLimit = setGasLimit(api, 3_200_000_000, 0); 
         const stakingContractFactory = new ConstructorsStakingNft(api, defaultSigner);
         
-        let adminAddress = defaultSigner.address;
-        let limitUnstakeTime = 1;
+        let stakingAdminAddress = defaultSigner.address;
+        let stakingLimitUnstakeTime = 1; // 1 min
         
         stakingContractAddress = (
             await stakingContractFactory.new(
-                adminAddress,
-                nftContractAddress,
-                limitUnstakeTime,
+                stakingAdminAddress,
+                nftPmpContractAddress,
+                stakingLimitUnstakeTime,
                 {gasLimit: stakingContractGasLimit}
             )
         ).address;
+
+        // console.log("stakingContractAddress =", stakingContractAddress);
+        
         stakingContract = new ContractStakingNft(stakingContractAddress, defaultSigner, api);
+        stakingQuery = stakingContract.query;
+        stakingTx = stakingContract.tx;
 
         /**
          * Step 4: Create  collection manager contract
          */
-        standardNftHash = psp34NftStandard.source.hash;
-        simpleModeAddingFee = "3000000000000";
-        advanceModeAddingFee = "5000000000000";
-        maxRoyaltyFeeRate = 500;
-        let collectionManagerContractGasLimit = setGasLimit(api, 3_200_000_000, 0);
+        cmAdminAddress = defaultSigner.address;
+        cmStandardNftHash = psp34NftStandard.source.hash;
+        cmSimpleModeAddingFee = "3000000000000";
+        cmAdvanceModeAddingFee = "5000000000000";
+        cmMaxRoyaltyFeeRate = 500;
+
+        // "refTime: 1556009780"
+        let cmContractGasLimit = setGasLimit(api, 3_200_000_000, 0);
         
-        const collectioManagerContractFactory = new ConstructorsCollectionManager(api, defaultSigner);
-        collectionManagerContractAddress = (
-            await collectioManagerContractFactory.new(
-                adminAddress,
-                standardNftHash,
-                simpleModeAddingFee,
-                advanceModeAddingFee,
-                maxRoyaltyFeeRate,
-                {gasLimit: collectionManagerContractGasLimit}
+        const cmContractFactory = new ConstructorsCollectionManager(api, defaultSigner);
+        cmContractAddress = (
+            await cmContractFactory.new(
+                cmAdminAddress,
+                cmStandardNftHash,
+                cmSimpleModeAddingFee,
+                cmAdvanceModeAddingFee,
+                cmMaxRoyaltyFeeRate,
+                {gasLimit: cmContractGasLimit}
             )
         ).address;
-        collectionManagerContract = new ContractCollectionManager(collectionManagerContractAddress, defaultSigner, api); 
+
+        // console.log("cmContractAddress =", cmContractAddress);       
+        
+        cmContract = new ContractCollectionManager(cmContractAddress, defaultSigner, api); 
+        cmQuery = cmContract.query;
+        cmTx = cmContract.tx;
 
         /**
-         * Step 5: Create marketplace contract
+         * Step 5: Alice and Bob create simple/advance collection and mint  
          */
-        let marketplaceGasLimit = setGasLimit(api, 3_200_000_000, 0); 
-        const marketplaceContractFactory = new ConstructorsMarketplace(api, defaultSigner);
+
+        // Alice creates collection in simple mode
+        let aliceNftName = "Simple mode collection";
+        let aliceNftSymbol = "SMC";
+        let aliceAttributes = ["Hair", "Color", "Type"];
+        let aliceAttributeVals = ["Straight", "Yellow", "Round"];
+        let aliceIsCollectRoyaltyFee = true;
+        let aliceRoyaltyFee = 400;
+
+        await cmContract.withSigner(alice).tx.autoNewCollection(
+            aliceNftName,
+            aliceNftSymbol,
+            aliceAttributes,
+            aliceAttributeVals,
+            aliceIsCollectRoyaltyFee,
+            aliceRoyaltyFee,
+            {value: cmSimpleModeAddingFee} 
+        );
+
+        aliceNftContractAddress = (await cmQuery.getContractById(1)).value.ok;
+        // console.log("aliceNftContractAddress =", aliceNftContractAddress);    
+
+        aliceNftContract = new ContractPsp34Nft(aliceNftContractAddress, alice, api);    
+        aliceNftQuery = aliceNftContract.query;
+        aliceNftTx = aliceNftContract.tx;     
         
-        adminAddress = defaultSigner.address;
+        // Active Alice's collection
+        await cmTx.updateIsActive(
+            aliceNftContractAddress,
+            true
+        );
+
+        // Alice mints NFT tokenid #1
+        await aliceNftTx.mint(); // Mint #1
+
+        // Bob creates collection in advance mode
+        // Bob creates a nft
+        let bobNftgasLimit = setGasLimit(api, 1_600_000_000, 0);
+        
+        let bobNftContractFactory = new ConstructorsPsp34Nft(api, bob);
+        let bobNftName = "Advanced mode collection";
+        let bobNftSymbol = "AMC";
+
+        bobNftContractAddress = (
+            await bobNftContractFactory.new(
+                bob.address,
+                bobNftName,
+                bobNftSymbol,
+                {gasLimit: bobNftgasLimit}
+            )
+        ).address;
+
+        // console.log("bobNftContractAddress =", bobNftContractAddress);
+
+        bobNftContract = new ContractPsp34Nft(bobNftContractAddress, bob, api);    
+        bobNftQuery = bobNftContract.query;
+        bobNftTx = bobNftContract.tx;      
+        
+        // Bob puts nft contract address to addNewCollection func
+        let bobAttributes = ["Processor", "RAM", "Thread", "Weight"];
+        let bobAttributeVals = ["Intel P4", "32 GB", "8", "2"];
+        let bobIsCollectRoyaltyFee = true;
+        let bobRoyaltyFee = 400;
+
+        await cmContract.withSigner(bob).tx.addNewCollection(
+            bobNftContractAddress,
+            bobAttributes,
+            bobAttributeVals,
+            bobIsCollectRoyaltyFee,
+            bobRoyaltyFee,
+            {value: cmAdvanceModeAddingFee} 
+        );
+        // console.log("Bob addNewCollection");
+    
+        // Active Bob's collection
+        await cmTx.updateIsActive(
+            bobNftContractAddress,
+            true
+        );
+        // console.log("cm updateIsActive Bob collection");
+        // Bob mints NFT tokenid #1
+        await bobNftTx.mint(); // Mint #1
+        // console.log("Bob mint NFT");
+
+        /**
+         * Step 6: Create marketplace contract
+         */
+        // "refTime: 961948207"
+        let gasLimit = setGasLimit(api, 2_000_000_000, 0); 
+        const contractFactory = new ConstructorsMarketplace(api, defaultSigner);
+      
         platformFee = 500;
 
         contractAddress = (
-            await marketplaceContractFactory.new(
-                collectionManagerContract,
+            await contractFactory.new(
+                cmContractAddress,
                 stakingContractAddress,
                 platformFee,
-                {gasLimit: marketplaceGasLimit}
+                {gasLimit}
             )
         ).address;
+
+        // console.log("contractAddress =", contractAddress);
+               
         contract = new ContractMarketplace(contractAddress, defaultSigner, api);
         query = contract.query;
         tx = contract.tx;
@@ -156,8 +290,42 @@ describe('Profile test', () => {
         await setup();
     });
 
-    it('Listing the NFT onto the marketplac', async () => {   
-        // Alice creates collection in simple mode
+    it('Can list NFT in marketplace', async () => {   
+        let nftTokenId;
+        let tokenId;
+        let price;
         
+        // Alice approves for marketplace contract to transfer NFT token #1
+        nftTokenId = 1;
+        tokenId = PSP34Args.IdBuilder.U64(nftTokenId); // Nft id 1
+        
+        await aliceNftTx.approve(
+            contractAddress,
+            tokenId,
+            true
+        );
+        
+        // Alice lists token #1        
+        price = 10000000000000; // 10 TZERO;        
+        await contract.withSigner(alice).tx.list(
+            aliceNftContractAddress, 
+            tokenId, 
+            price
+        );
+        
+        let count = (await query.getSaleTokensIdsCount(
+            aliceNftContractAddress,
+            alice.address
+        )).value.ok!.rawNumber.toString();
+        
+        expect(count).to.equal("1");
+        
+        // let rTokenId = await query.getForSaleTokenId(
+        //     aliceNftContractAddress,
+        //     alice.address,
+        //     new BN(1)
+        // );
+        
+        // console.log("rTokenId = ", rTokenId);
     });
 });
