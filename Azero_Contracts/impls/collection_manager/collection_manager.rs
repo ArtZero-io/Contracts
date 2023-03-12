c67f92ce007cfd6774728a9aba7448ac7892177c
Albert Tran <alberttran0810@gmail.com>
2023-03-13 02:08:24 +0700

    Feature: replace unwrap function in collection trait

--- Azero_Contracts/impls/collection_manager/collection_manager.rs
+++ Azero_Contracts/impls/collection_manager/collection_manager.rs
@@ -22,31 +22,31 @@ pub const ADMINER: RoleType = ink::selector_id!("ADMINER");
 impl<T: Storage<Manager>> ArtZeroCollectionTrait for T {
 
     default fn get_royalty_fee(&self, nft_contract_address: AccountId) -> u32 {
-        if self.data::<Manager>().collections.get(&nft_contract_address).is_none() {
-            return 0
-        }
-        let collection = self.data::<Manager>().collections.get(&nft_contract_address).unwrap();
-        if !collection.is_collect_royalty_fee || !collection.is_active {
-            return 0
+        if let Some(collection) = self.data::<Manager>().collections.get(&nft_contract_address) {
+            if !collection.is_collect_royalty_fee || !collection.is_active {
+                return 0
+            } else {
+                collection.royalty_fee
+            }
         } else {
-            collection.royalty_fee
+            return 0;
         }
     }
 
     default fn is_active(&self, nft_contract_address: AccountId) -> bool {
-        if self.data::<Manager>().collections.get(&nft_contract_address).is_none() {
+        if let Some(collection) =  self.data::<Manager>().collections.get(&nft_contract_address) {
+            return collection.is_active
+        } else {
             return false
         }
-        let collection = self.data::<Manager>().collections.get(&nft_contract_address).unwrap();
-        return collection.is_active
     }
 
     default fn get_contract_type(&self, nft_contract_address: AccountId) -> CollectionType {
-        if self.data::<Manager>().collections.get(&nft_contract_address).is_none() {
+        if let Some(collection) = self.data::<Manager>().collections.get(&nft_contract_address) {
+            collection.contract_type
+        } else {
             return CollectionType::Unknown
         }
-        let collection = self.data::<Manager>().collections.get(&nft_contract_address).unwrap();
-        return collection.contract_type
     }
 
     default fn get_collection_owner(&self, nft_contract_address: AccountId) -> Option<AccountId> {
