5b0df06c9a4faa92fd48a867fd47a77e9b5a7382
Albert Tran <alberttran0810@gmail.com>
2023-03-16 00:56:14 +0700

    Feature: move get set functions to trait of staking contract

--- Azero_Contracts/src/impls/staking/staking.rs
+++ Azero_Contracts/src/impls/staking/staking.rs
@@ -14,8 +14,11 @@ use openbrush::{
     traits::{
         Storage,
         AccountId,
+        Balance,
+        Hash
     }
 };
+
 use crate::traits::error::*;
 
 /// Throws if is_locked is false
@@ -58,5 +61,65 @@ impl<T: Storage<Manager>> ArtZeroStakingTrait for T {
     /// Get User NFT staked in the contract
     default fn get_total_pending_unstaked_by_account(&self, account: AccountId) -> u64 {
         return self.data::<Manager>().pending_unstaking_list.count(account) as u64;
+    }
+
+    default fn get_reward_pool(&self) -> Balance {
+        self.data::<Manager>().reward_pool
+    }
+    
+    default fn get_claimable_reward(&self) -> Balance {
+        self.data::<Manager>().claimable_reward
+    }
+    
+    default fn is_claimed(&self, account: AccountId) -> bool {
+        self.data::<Manager>().is_claimed.get(&account).unwrap_or(false)
+    }
+    
+    default fn get_reward_started(&self) -> bool {
+        self.data::<Manager>().reward_started
+    }
+    
+    default fn get_artzero_nft_contract(&self) -> AccountId {
+        self.data::<Manager>().nft_contract_address
+    }
+    
+    default fn get_is_locked(&self) -> bool {
+        self.data::<Manager>().is_locked
+    }
+    
+    default fn get_admin_address(&self) -> AccountId {
+        self.data::<Manager>().admin_address
+    }
+    
+    default fn get_request_unstake_time(&self, account: AccountId, token_id: u64) -> u64 {
+       self.data::<Manager>().request_unstaking_time.get(&(&account, &token_id)).unwrap_or(0)
+    }
+    
+    default fn get_staked_id(&self, account: AccountId, index: u64) -> Option<u64> {
+        self.data::<Manager>().staking_list.get_value(account, &(index as u128))
+    }
+    
+    default fn get_staked_accounts_index_by_account(&self, account: AccountId) -> Option<u128> {
+        self.data::<Manager>().staked_accounts.get_index(0, &account)
+    }
+    
+    default fn get_staked_accounts_account_by_index(&self, index: u64) -> Option<AccountId> {
+        self.data::<Manager>().staked_accounts.get_value(0, &(index as u128))
+    }
+    
+    default fn get_staked_accounts_last_index(&self) -> u64 {
+        self.data::<Manager>().staked_accounts.count(0) as u64
+    }
+    
+    default fn get_pending_unstaked_id(&self, account: AccountId, index: u64) -> Option<u64> {
+        self.data::<Manager>().pending_unstaking_list.get_value(account, &(index as u128))
+    }
+    
+    default fn get_total_staked(&self) -> u64 {
+        self.data::<Manager>().total_staked
+    }
+    
+    default fn get_limit_unstake_time(&self) -> u64 {
+        self.data::<Manager>().limit_unstake_time
     }
 }
--- Azero_Contracts/src/traits/staking.rs
+++ Azero_Contracts/src/traits/staking.rs
@@ -1,6 +1,11 @@
 use openbrush::{
+    modifier_definition,
+    contracts::access_control::*,
     traits::{
+        Storage,
         AccountId,
+        Balance,
+        Hash
     }
 };
 
@@ -15,4 +20,49 @@ pub trait ArtZeroStakingTrait {
     /// This function returns the total PMP NFT that is pending to be unstaked by an account
     #[ink(message)]
     fn get_total_pending_unstaked_by_account(&self, account: AccountId) -> u64;
+
+    #[ink(message)]
+    fn get_reward_pool(&self) -> Balance;
+
+    #[ink(message)]
+    fn get_claimable_reward(&self) -> Balance;
+
+    #[ink(message)]
+    fn is_claimed(&self, account: AccountId) -> bool;
+
+    #[ink(message)]
+    fn get_reward_started(&self) -> bool;
+
+    #[ink(message)]
+    fn get_artzero_nft_contract(&self) -> AccountId;
+
+    #[ink(message)]
+    fn get_is_locked(&self) -> bool;
+
+    #[ink(message)]
+    fn get_admin_address(&self) -> AccountId;
+
+    #[ink(message)]
+    fn get_request_unstake_time(&self, account: AccountId, token_id: u64) -> u64;
+
+    #[ink(message)]
+    fn get_staked_id(&self, account: AccountId, index: u64) -> Option<u64>;
+
+    #[ink(message)]
+    fn get_staked_accounts_index_by_account(&self, account: AccountId) -> Option<u128>;
+
+    #[ink(message)]
+    fn get_staked_accounts_account_by_index(&self, index: u64) -> Option<AccountId>;
+
+    #[ink(message)]
+    fn get_staked_accounts_last_index(&self) -> u64;
+
+    #[ink(message)]
+    fn get_pending_unstaked_id(&self, account: AccountId, index: u64) -> Option<u64>;
+
+    #[ink(message)]
+    fn get_total_staked(&self) -> u64;
+
+    #[ink(message)]
+    fn get_limit_unstake_time(&self) -> u64;
 }
