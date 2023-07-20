6db27badf82d2d8c0aea52ecb5d08331cc7d5d11
Albert Tran <alberttran0810@gmail.com>
2023-07-21 00:48:16 +0700

    Add Psp34Traits to PSP34 Standard Contract

--- Standard_Contracts/contracts/psp34_standard/lib.rs
+++ Standard_Contracts/contracts/psp34_standard/lib.rs
@@ -7,6 +7,7 @@ pub use self::psp34_nft::{
 #[openbrush::implementation(PSP34, Ownable, PSP34Metadata, PSP34Enumerable)]
 #[openbrush::contract]
 pub mod psp34_nft {
+    use standard_contracts::impls::psp34_standard::*;
     use standard_contracts::traits::psp34_standard::*;
     use openbrush::{
         modifiers,
@@ -67,6 +68,71 @@ pub mod psp34_nft {
             } else {
                 return Err(PSP34Error::Custom(String::from("No token owner found")));
             }
+        }
+    }
+
+    impl Psp34TraitsImpl for Psp34Nft {}
+
+    impl Psp34Traits for Psp34Nft {
+        /// This function sets the baseURI for the NFT contract. Only Contract Owner can perform this function. baseURI is the location of the metadata files if the NFT collection use external source to keep their NFT artwork. ArtZero uses IPFS by default, the baseURI can have format like this: ipfs://<hash_ID>/
+        #[ink(message)]
+        fn set_base_uri(&mut self, uri: String) -> Result<(), Error> {
+            Psp34TraitsImpl::set_base_uri(self, uri)
+        }
+
+        /// This function set the attributes to each NFT. Only Contract Owner can perform this function. The metadata input is an array of [(attribute, value)]. The attributes in ArtZero platform are the NFT traits.
+        #[ink(message)]
+        fn set_multiple_attributes(
+            &mut self,
+            token_id: Id,
+            metadata: Vec<(String, String)>
+        ) -> Result<(), Error> {
+            Psp34TraitsImpl::set_multiple_attributes(self, token_id, metadata)
+        }
+        /// This function returns all available attributes of each NFT
+        #[ink(message)]
+        fn get_attributes(&self, token_id: Id, attributes: Vec<String>) -> Vec<String> {
+            Psp34TraitsImpl::get_attributes(self, token_id, attributes)
+        }
+        /// This function return how many unique attributes in the contract
+        #[ink(message)]
+        fn get_attribute_count(&self) -> u32 {
+            Psp34TraitsImpl::get_attribute_count(self)
+        }
+        /// This function return the attribute name using attribute index. Beacause attributes of an NFT can be set to anything by Contract Owner, AztZero uses this function to get all attributes of an NFT
+        #[ink(message)]
+        fn get_attribute_name(&self, index: u32) -> String {
+            Psp34TraitsImpl::get_attribute_name(self, index)
+        }
+        /// This function return the metadata location of an NFT. The format is baseURI/<token_id>.json
+        #[ink(message)]
+        fn token_uri(&self, token_id: u64) -> String {
+            Psp34TraitsImpl::token_uri(self, token_id)
+        }
+        /// This function return the owner of the NFT Contract
+        #[ink(message)]
+        fn get_owner(&self) -> Option<AccountId> {
+            Psp34TraitsImpl::get_owner(self)
+        }
+        /// This function return the latest token ID, everytime new NFT is mint, last_token_id is increased by 1 in mint function. Note: This is not the same as the total supply return by the psp34 function as NFT can be burnt.
+        #[ink(message)]
+        fn get_last_token_id(&self) -> u64 {
+            Psp34TraitsImpl::get_last_token_id(self)
+        }
+        /// This function lets NFT owner to lock their NFT. Once locked, the NFT traits (attributes) can not be changed
+        #[ink(message)]
+        fn lock(&mut self, token_id: Id) -> Result<(), Error> {
+            Psp34TraitsImpl::lock(self, token_id)
+        }
+        /// This function check if an NFT is locked or not
+        #[ink(message)]
+        fn is_locked_nft(&self, token_id: Id) -> bool {
+            Psp34TraitsImpl::is_locked_nft(self, token_id)
+        }
+        /// This function returns how many NFTs have been locked by its owners
+        #[ink(message)]
+        fn get_locked_token_count(&self) -> u64 {
+            Psp34TraitsImpl::get_locked_token_count(self)
         }
     }
 
